34d69ff3e7db737e38aa2102348c8b206b7835fe
TharitT <tharit.tangkij@gmail.com>
2024-12-22 21:21:11 +0700

    problem 16 part1, part 2 almost..

--- day16/day16.py
+++ day16/day16.py
@@ -19,13 +19,13 @@ def get_start_xy(grid):
                 return j,i 
 def get_possible_turn(facing):
     if facing == 'E':
-        return [(0,-1, 'N'), (1,0,'E'), (0,1,'S')]
+        return [(0, -1, 'N'), (1, 0,'E'), (0, 1,'S')]
     elif facing == 'N':
-        return [(-1,0, 'W'), (1,0,'E'), (0,-1,'N')]
+        return [(-1, 0, 'W'), (1, 0,'E'), (0, -1,'N')]
     elif facing == 'W':
-        return [(0,-1, 'N'), (-1,0,'W'), (0,1,'S')]
+        return [(0,-1,'N'), (-1, 0,'W'), (0, 1,'S')]
     elif facing == 'S':
-        return [(0,1, 'S'), (1,0,'E'), (-1,0,'W')]
+        return [(0, 1,'S'), (1, 0,'E'), (-1, 0,'W')]
 
 def check_valid_xy(grid, x, y):
     if grid[y][x] == '#':
@@ -35,38 +35,77 @@ def check_valid_xy(grid, x, y):
     else:
         return True
 
-def find_all_paths(grid, x0, y0):
-    visited = [ [False for i in range(len(grid[0]))] for j in range(len(grid))]
-    pathScore = []
-    memo = {}
-    def dfs(grid, x, y, facing, score, visited, minScore,memo):
-        if grid[y][x] == 'E':
-            minScore[0] = min(minScore[0], score)
-            print("So far", minScore)
-            return minScore[0]
-        if not check_valid_xy(grid, x, y):
-            return
-        if visited[y][x]:
-            return
-        if score > minScore[0]:
-            return
+def find_all_paths_bfs(grid, x0, y0):
+    visited = [ [float('inf') for i in range(len(grid[0]))] for j in range(len(grid))]
+    q = [(x0, y0, 'E', 0)]
+    minScore = float('inf')
+    while q:
+        x, y, facing, score = q.pop(0)
+        if score > visited[y][x]:
+            continue
         else:
-            visited[y][x] = True
-            turns = get_possible_turn(facing)
-            for dx, dy, nextTurn in turns:
-                if nextTurn == facing:
-                    s = dfs(grid, x+dx, y+dy, nextTurn, score+1, visited, minScore)
-                else:
-                    s = dfs(grid, x+dx, y+dy, nextTurn, score+1001, visited, minScore)
-            visited[y][x] = False
-    minScore = [float('inf')]
-    dfs(grid, x0, y0, 'E', 0, visited, minScore)
-    print("min path score: ", minScore[0]) 
-    return pathScore
+            visited[y][x] = score
 
-import sys
+        if grid[y][x] == 'E':
+            minScore = score
+        turns = get_possible_turn(facing)
+        for dx, dy, nextTurn in turns:
+            if check_valid_xy(grid, x+dx, y+dy):
+                new_score = score + (1001 if nextTurn != facing else 1)
+                q.append((x+dx, y+dy, nextTurn, new_score))
+    print("Part 1: minScore is: ", minScore)
+    return minScore
+
+import heapq
+def find_opt_paths(grid, x0, y0):
+    visited = [ [float('inf') for i in range(len(grid[0]))] for j in range(len(grid))]
+    q = [(x0, y0, 'E', 0, [(x0,y0)])]
+    minScore = float('inf')
+    pathSet = set()
+    while q:
+        x, y, facing, score, path = q.pop(0)
+        #print(x, y, facing, score, path)
+        if score > visited[y][x]:
+            continue
+        else:
+            visited[y][x] = score
+        # Check if we've reached the endpoint
+        if grid[y][x] == 'E':
+            if score < minScore:
+                pathSet = set()
+                minScore = score
+                print("reset pathSet with new minimum score:", minScore)
+            for e in path:
+                pathSet.add(e)
+            print("Current pathSet size:", len(pathSet))
+            continue
+        turns = get_possible_turn(facing)
+        for dx, dy, nextTurn in turns:
+            if check_valid_xy(grid, x+dx, y+dy):
+                new_score = score + (1001 if nextTurn != facing else 1)
+                new_path = [e for e in path]
+                new_path.append((x+dx, y+dy))
+                #heapq.heappush(q, (x+dx, y+dy, nextTurn, new_score, new_path))
+                q.append((x+dx, y+dy, nextTurn, new_score, new_path))
+    print("Part2 min Score is:", minScore, "elem in set", len(pathSet))
+    return minScore, pathSet
+
+def print_grid(grid, pathSet):
+    for j in range(len(grid)):
+        for i in range(len(grid[0])):
+
+            if (i, j) in pathSet:
+                print("O", end="")
+            else:
+                if i == len(grid[0]) - 1:
+                    print(grid[j][i])
+                else: 
+                    print(grid[j][i], end="")
+
 if __name__ == "__main__":
-    sys.setrecursionlimit(5000)
     grid = read_input()
     x0, y0 = get_start_xy(grid)
-    pathScore = find_all_paths(grid, x0, y0)
+    minScore = find_all_paths_bfs(grid, x0, y0)
+    minScore, pathSet = find_opt_paths(grid, x0, y0)
+
+    #print_grid(grid, pathSet)
